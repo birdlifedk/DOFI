d8f34a9f6e4633873444c670ea06db092fdcbb9b
AndreasDOF <andreas.dahl@dof.dk>
2015-04-12 23:21:24 +0200

    login is working now

--- DOFI/DOFIService.swift
+++ DOFI/DOFIService.swift
@@ -11,20 +11,56 @@ import Foundation
 class DOFIService: WebserviceProtocol {
 
 	func login(username: NSString, password:NSString) {
-		//var params = ["username":username, "password":password, "grant_type":"password", "client_id": "2", "client_secret" : "DOFISECRET"] as Dictionary<String, String>
-		//var url:NSURL = NSURL(string: "http://dev.dofbasenweb/login")!
+		let params = ["username":username, "password":password, "grant_type":"password", "client_id": "2", "client_secret" : "DOFISECRET"] as Dictionary<String, String>
+		var url:NSURL = NSURL(string: "http://dev.dofbasenweb/login")!
 
-		//var err: NSError?
-		//var request:NSMutableURLRequest = NSMutableURLRequest(URL: url)
-		//request.HTTPMethod = "POST"
+		var err: NSError?
+		var request:NSMutableURLRequest = NSMutableURLRequest(URL: url)
+		request.HTTPMethod = "POST"
+		request.HTTPBody = NSJSONSerialization.dataWithJSONObject(params, options: nil, error: &err)
+		request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+		request.setValue("application/json", forHTTPHeaderField: "Accept")
 
-		//request.HTTPBody = NSJSONSerialization.dataWithJSONObject(params, options: nil, error: &err)
-		//request.setValue("application/json", forHTTPHeaderField: "Content-Type")
-		//request.setValue("application/json", forHTTPHeaderField: "Accept")
 
+		var response: NSURLResponse?
+		var responseError: NSError?
 
-		//var response: NSURLResponse?
+		var urlData: NSData? = NSURLConnection.sendSynchronousRequest(request, returningResponse:&response, error:&err)
 
-		//var urlData: NSData? = NSURLConnection.sendSynchronousRequest(request, returningResponse:&response, error:&err)
+		if ( urlData != nil ) {
+			var res = response as NSHTTPURLResponse;
+
+			if (res.statusCode >= 200 && res.statusCode < 300) {
+				var responseData:NSString  = NSString(data:urlData!, encoding:NSUTF8StringEncoding)!
+				let jsonData:NSDictionary = NSJSONSerialization.JSONObjectWithData(urlData!, options:NSJSONReadingOptions.MutableContainers , error: &responseError) as NSDictionary
+				let accessToken:NSString = jsonData.valueForKey("access_token") as NSString
+				let refreshToken:NSString = jsonData.valueForKey("refresh_token") as NSString
+				let expiresIn:Double = jsonData.valueForKey("expires_in") as Double
+
+				var token = Token(accessToken: accessToken, refreshToken: refreshToken, expiresIn: expiresIn)
+
+				url = NSURL(string: "http://dev.dofbasenweb/user/\(username)?access_token=\(token.accessToken)")!
+				request = NSMutableURLRequest(URL: url)
+				request.HTTPMethod = "GET"
+				urlData = NSURLConnection.sendSynchronousRequest(request, returningResponse:&response, error:&responseError)
+				if ( urlData != nil ) {
+					res = response as NSHTTPURLResponse;
+					if (res.statusCode >= 200 && res.statusCode < 300) {
+						var responseData:NSString  = NSString(data:urlData!, encoding:NSUTF8StringEncoding)!
+						let jsonDataa:NSDictionary = NSJSONSerialization.JSONObjectWithData(urlData!, options:NSJSONReadingOptions.MutableContainers , error: &responseError) as NSDictionary
+
+						var user = User(id: 1 as NSInteger, name: "Andreas" as NSString, surname: "Sørensen" as NSString)
+						Session.setUser(user)
+						Session.setLoggedInStatus(true)
+						//self.performSegueWithIdentifier("gotoWelcome", sender: self)
+
+						var prefs:NSUserDefaults = NSUserDefaults.standardUserDefaults()
+						prefs.setObject(Session.getUser().getUsername(), forKey: "USERNAME")
+						prefs.setBool(Session.isLoggedIn(), forKey: "ISLOGGEDIN")
+						prefs.synchronize()
+					}
+				}
+			}
+		}
 	}
 }
