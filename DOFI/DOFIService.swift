5a91263cb0209e6d7c1e8a87ccbbc74b0c12fd26
Rasmus Rosted <Rosted@Rasmuss-Air.domain_not_set.invalid>
2015-04-14 15:09:51 +0200

    post pull + merge

--- DOFI/DOFIService.swift
+++ DOFI/DOFIService.swift
@@ -10,12 +10,9 @@ import Foundation
 
 class DOFIService: WebserviceProtocol {
 
-	func login(username: NSString, password:NSString) -> (ReturnMessage, User, [Trip]) {
-        var returnMessage = ReturnMessage(message: "", isDone: true)
-        var user = User(name: "name", surname: "surname")
-        var trips = [Trip(), Trip()]
-		//var params = ["username":username, "password":password, "grant_type":"password", "client_id": "2", "client_secret" : "DOFISECRET"] as Dictionary<String, String>
-		//var url:NSURL = NSURL(string: "http://dev.dofbasenweb/login")!
+	func login(username: NSString, password:NSString) {
+		let params = ["username":username, "password":password, "grant_type":"password", "client_id": "2", "client_secret" : "DOFISECRET"] as Dictionary<String, String>
+		var url:NSURL = NSURL(string: "http://dev.dofbasenweb/login")!
 
 		var err: NSError?
 		var request:NSMutableURLRequest = NSMutableURLRequest(URL: url)
@@ -30,8 +27,40 @@ class DOFIService: WebserviceProtocol {
 
 		var urlData: NSData? = NSURLConnection.sendSynchronousRequest(request, returningResponse:&response, error:&err)
 
-		//var urlData: NSData? = NSURLConnection.sendSynchronousRequest(request, returningResponse:&response, error:&err)
-        
-        return (returnMessage, user, trips)
+		if ( urlData != nil ) {
+			var res = response as NSHTTPURLResponse;
+
+			if (res.statusCode >= 200 && res.statusCode < 300) {
+				var responseData:NSString  = NSString(data:urlData!, encoding:NSUTF8StringEncoding)!
+				let jsonData:NSDictionary = NSJSONSerialization.JSONObjectWithData(urlData!, options:NSJSONReadingOptions.MutableContainers , error: &responseError) as NSDictionary
+				let accessToken:NSString = jsonData.valueForKey("access_token") as NSString
+				let refreshToken:NSString = jsonData.valueForKey("refresh_token") as NSString
+				let expiresIn:Double = jsonData.valueForKey("expires_in") as Double
+
+				var token = Token(accessToken: accessToken, refreshToken: refreshToken, expiresIn: expiresIn)
+
+				url = NSURL(string: "http://dev.dofbasenweb/user/\(username)?access_token=\(token.accessToken)")!
+				request = NSMutableURLRequest(URL: url)
+				request.HTTPMethod = "GET"
+				urlData = NSURLConnection.sendSynchronousRequest(request, returningResponse:&response, error:&responseError)
+				if ( urlData != nil ) {
+					res = response as NSHTTPURLResponse;
+					if (res.statusCode >= 200 && res.statusCode < 300) {
+						var responseData:NSString  = NSString(data:urlData!, encoding:NSUTF8StringEncoding)!
+						let jsonDataa:NSDictionary = NSJSONSerialization.JSONObjectWithData(urlData!, options:NSJSONReadingOptions.MutableContainers , error: &responseError) as NSDictionary
+
+						var user = User(id: 1 as NSInteger, name: "Andreas" as NSString, surname: "Sørensen" as NSString)
+						Session.setUser(user)
+						Session.setLoggedInStatus(true)
+						//self.performSegueWithIdentifier("gotoWelcome", sender: self)
+
+						var prefs:NSUserDefaults = NSUserDefaults.standardUserDefaults()
+						prefs.setObject(Session.getUser().getUsername(), forKey: "USERNAME")
+						prefs.setBool(Session.isLoggedIn(), forKey: "ISLOGGEDIN")
+						prefs.synchronize()
+					}
+				}
+			}
+		}
 	}
 }
