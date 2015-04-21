f0da2d16e14f83218ae75d4e99819627941d44f9
Rasmus Rosted <Rosted@Rasmuss-Air.domain_not_set.invalid>
2015-04-21 19:21:35 +0200

    First version of mappers created

---
+++ DOFI/TripMapper.swift
@@ -0,0 +1,18 @@
+//
+//  TripMapper.swift
+//  DOFI
+//
+//  Created by Rasmus Rosted on 21/04/15.
+//  Copyright (c) 2015 DOF. All rights reserved.
+//
+
+import Foundation
+import Realm
+
+class TripMapper {
+    
+    func getTrip(rlmObject: RLMObject) -> Trip
+    {
+        return Trip()
+    }
+}
